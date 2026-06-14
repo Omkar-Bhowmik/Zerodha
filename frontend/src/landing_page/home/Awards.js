0c87db7cf2dcf91126a54cba3afda10007ff9819
Omkar-Bhowmik <omkarbhowmik7@gmail.com>
2026-06-15 03:31:35 +0530

    Update: Added Hero component

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -1,7 +1,16 @@
 import React from 'react';
 function Awards() {
      return (  
-          <h1>Awards</h1>
+          <div className='container mt-5'>
+               <div className='row'>
+                    <div className='col-6'>
+                         <img src= 'media/images/largestBroker.svg' alt='largest broker'/>
+                    </div>
+                    <div className='col-6'>
+                         <h1>Lardest stock broker in India</h1>
+                    </div>
+               </div>
+          </div>
      );
 }
 
